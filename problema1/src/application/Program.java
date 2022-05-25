e9cee4dd0c47717fd207d9a803fd7c8913da3616
leandrobarr <leandrobarr@yahoo.com.br>
2022-05-25 16:20:30 -0300

    Introdução aos generics

---
+++ problema1/src/application/Program.java
@@ -0,0 +1,32 @@
+package application;
+
+import java.util.Scanner;
+
+import services.PrintService;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		PrintService<String> ps = new PrintService<>();
+
+		System.out.print("How many values? ");
+		int n = sc.nextInt();
+		
+		//ps.addValue("Maria");
+
+		for (int i = 0; i < n; i++) {
+			String value = sc.next();
+			ps.addValue(value);
+		}
+
+		ps.print();
+		String x = ps.first();
+		System.out.println("First: " + ps.first());
+
+		sc.close();
+	}
+
+}
